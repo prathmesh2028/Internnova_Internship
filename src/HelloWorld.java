94a3409a25c67e7ad125be2f615ff469d4b42c0a
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-03 18:56:50 +0530

    Task 1: hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
+        System.out.println("Hello there welcome to my source code :)");
+        System.out.println("My name is PRATHMESH BADHE");
+        System.out.println("My college name is Sinhgad College , Pune");
+        System.out.println("My branch is Information Technology");
     }
 }
