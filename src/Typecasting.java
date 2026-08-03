94dc01a4ae1218ef47fde682831528ecab1940d2
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-03 19:32:05 +0530

    Task 5:Type Casting

---
+++ src/Typecasting.java
@@ -0,0 +1,17 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        // type casting
+
+        System.out.println("Explicit type casting");
+        double a = 100.43;
+        int x = (int)a;
+        System.out.println(x);
+        System.out.println(a);
+
+        System.out.println("\nImplicit type casting");
+        int b = 43;
+        double y = b;
+        System.out.println(b);
+        System.out.println(y);
+    }
+}
