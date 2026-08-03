6840b167c0c94ec20bb010c0bcc27a5c93f07696
PRATHMESH BADHE <badheprathmesh2028@gmail.com>
2026-08-03 19:03:40 +0530

    Task 2:Variables and Datatypes

---
+++ src/VariablesAndDatatypes.java
@@ -0,0 +1,21 @@
+public class VariablesAndDatatypes {
+    public static void main(String[] args) {
+
+        String name = "Prathmesh";
+        int age = 20;
+        double height = 53.5;
+        double weight = 20;
+        double CGPA = 9.3;
+        String Gender = "Male";
+        boolean isStudent = true;
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(CGPA);
+        System.out.println(Gender);
+        System.out.println(isStudent);
+    }
+}
